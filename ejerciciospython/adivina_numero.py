071c5e5f4733e03523d7d33d74fb68eed4732aa9
jamil-camayo-code <camayojamil@gmail.com>
2026-06-30 11:12:52 -0500

    first commit

---
+++ ejerciciospython/adivina_numero.py
@@ -0,0 +1,8 @@
+#El jugador debe ingresar un numero para tratar de adividar un numero ganador.
+numero_ganador=8
+numero_juagador=int(input("Ingrese un numero del 1 al 10 para adividar: "))
+
+if numero_ganador==numero_juagador:
+    print("Adivinaste el número")
+else:
+    print("No adivinaste")
